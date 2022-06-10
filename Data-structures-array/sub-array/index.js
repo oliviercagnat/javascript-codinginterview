f4d48e34d10524b7e5b8bb18022b1d21d67bb8ea
oliviercagnat <oliviercagnat@gmail.com>
2022-06-10 19:13:25 +0900

    add exo

--- Data-structures-array/sub-array/index.js
+++ Data-structures-array/sub-array/index.js
@@ -19,7 +19,7 @@ const maxSubArray = (nums) => {
     let maxSum = -Infinity;
     let currentSum = 0;
     // iterate through the nums, store sub-problems result
-    for(let i = 0; i < nums.length; i++){ 
+    for(let i = 0; i < nums.length; i++) { 
         //cumulating answers to the top
         
         //compare currentSum add current number 
